f658688a85226d1e084da25673619aa2f514bff7
EAMON MC CANN <10348373@brn004-linux.(none)>
2013-11-05 16:41:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetworkingSite::Application.config.secret_token = '5ff65461cf59ec2acc38c968b9fd1d7a8c7eaa4570be15f7bab1066ad11ab9121e166734ea0e29c6e206500d7b058ace65be219279dddb38e72ff1c48fbbebec'
